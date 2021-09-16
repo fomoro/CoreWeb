eb7475c76c34c8ba68b0ebd7141add627c01215b
fomoro <fomoro@gmail.com>
2021-09-15 23:38:13 -0500

    Se crea proyecto y solucion web

---
+++ ListaCursos/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListaCursos.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = Guid.NewGuid().ToString();
+        }
+    }
+}
