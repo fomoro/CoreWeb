d67041015cead005ea9452ded31be07eb6d9d21d
fomoro <fomoro@gmail.com>
2021-09-21 14:26:47 -0500

    ayuste de conexcion entre proyecto web consumiendo  webapi .net core 5

--- ApiCursos/Startup.cs
+++ ApiCursos/Startup.cs
@@ -1,17 +1,20 @@
-using ListaCursos.Interfaces;
-using ListaCursos.Providers;
+using ApiCursos.Interfaces;
+using ApiCursos.Providers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ListaCursos
+namespace ApiCursos
 {
     public class Startup
     {
@@ -22,30 +25,28 @@ namespace ListaCursos
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddHttpClient("coursesService", c => {
-               c.BaseAddress = new Uri(Configuration["CoursesService"]);
+        {           
+            services.AddSingleton<ICoursesProvider, FakeCoursesProvider>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCursos", Version = "v1" });
             });
-            services.AddSingleton<ICoursesProvider, WebApiCoursesProvider>();
-            //services.AddSingleton<ICoursesProvider, FakeCoursesProvider>();
-            services.AddRazorPages();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiCursos v1"));
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -53,7 +54,7 @@ namespace ListaCursos
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
