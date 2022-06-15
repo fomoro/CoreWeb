a8082eac7ac7a9c2b00a7d007daa87a0a775cb21
Jonathan Wolfan <wmoreno@viajemos.com>
2022-06-15 17:45:22 -0500

    se suben cursos

---
+++ Cursos/ApiCursos/Interfaces/ICoursesProvider.cs
@@ -0,0 +1,21 @@
+﻿using ApiCursos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCursos.Interfaces
+{
+    public interface ICoursesProvider
+    {
+        Task<ICollection<Course>> GetAllAsync();
+
+        Task<ICollection<Course>> SearchAsync(string search);
+
+        Task<Course> GetAsync(int id);
+
+        Task<bool> UpdateAsync(int id, Course course);
+
+        Task<(bool IsSuccess, int? Id)> AddAsync(Course course);
+    }
+}
